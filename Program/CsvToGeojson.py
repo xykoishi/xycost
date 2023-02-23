dedb676aebe0e446fcd9f689b11ab89c734cdd82
taroyaki <62540660+taroyaki@users.noreply.github.com>
2023-02-23 12:47:33 +0900

    0223

--- Program/CsvToGeojson.py
+++ Program/CsvToGeojson.py
@@ -1,15 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
 from geojson import Point, Feature, FeatureCollection, dump
 import csv
+
+
+# In[20]:
+
 
 CsvFile = 'Mybills.csv'
 DictFile = 'Mybills.txt'   # 中间文件
 GeojsonFile = 'Mybills.geojson'
 
+path = '/Users/xychen/Library/Mobile Documents/com~apple~CloudDocs/生活/家財管理/xycost/Data/'
+
+# In[21]:
+
+
 # Generate the intermid file for checking
-fo_csv = open(CsvFile, 'r', encoding='utf-8')
+fo_csv = open(path+CsvFile, 'r', encoding='utf-8')
 PointsData = csv.DictReader(fo_csv)
 
-with open(DictFile,'w') as fw_dict:
+with open(path+DictFile,'w') as fw_dict:
     for row in PointsData:
         #print(row)
         fw_dict.write(str(row))
@@ -17,7 +32,7 @@ with open(DictFile,'w') as fw_dict:
 fw_dict.close()
 
 # Generate the final Geojson file
-fo_csv = open(CsvFile, 'r', encoding='utf-8')   # 还要再写一次，否则下面的程序运行后输出为空，why？
+fo_csv = open(path+CsvFile, 'r', encoding='utf-8')   # 还要再写一次，否则下面的程序运行后输出为空，why？
 PointsData = csv.DictReader(fo_csv)
 
 ft_all = []
@@ -31,7 +46,8 @@ for i, p in enumerate(PointsData):
     ft_all.append(ft)
 ft_colct = FeatureCollection(ft_all)
 
-with open(GeojsonFile, 'w') as fw_g:
+with open(path+GeojsonFile, 'w') as fw_g:
     dump(ft_colct, fw_g, indent=2)
 fw_g.close()
 fo_csv.close()
+
