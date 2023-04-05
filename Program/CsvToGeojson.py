9c1105440575e7e2c4b0a4f8e515f402883e2f43
taroyaki <62540660+taroyaki@users.noreply.github.com>
2023-04-05 12:52:50 +0900

    geojson 修改

--- Program/CsvToGeojson.py
+++ Program/CsvToGeojson.py
@@ -39,7 +39,7 @@ ft_all = []
 for i, p in enumerate(PointsData):
     lat, lon = p['纬度'], p['经度']
     ft = Feature(geometry = Point((float(lon), float(lat),)),
-         properties = {'year': p['年'], 'month': p['月'], 'day': p['日'],
+         properties = {'year': int(p['年']), 'month': int(p['月']), 'day': int(p['日']),
                         'category': p['类别'], 'description': p['描述'], 'amount': p['金额'],
                         #'marker-size': 'medium','marker-symbol': ''
                         })
