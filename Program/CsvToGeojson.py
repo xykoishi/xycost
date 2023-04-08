1f54c71e5c23a60e26a1f531712827d06a843d1c
taroyaki <62540660+taroyaki@users.noreply.github.com>
2023-04-08 15:58:46 +0900

    new pathway

--- Program/CsvToGeojson.py
+++ Program/CsvToGeojson.py
@@ -12,10 +12,9 @@ import csv
 
 
 CsvFile = 'Mybills.csv'
-DictFile = 'Mybills.txt'   # 中间文件
 GeojsonFile = 'Mybills.geojson'
 
-path = '/Users/xychen/Library/Mobile Documents/com~apple~CloudDocs/生活/家財管理/xycost/Data/'
+path = '/Users/xychen/Library/Mobile Documents/com~apple~CloudDocs/eCXY/家財管理/xycost/Data/'
 
 # In[21]:
 
@@ -23,13 +22,6 @@ path = '/Users/xychen/Library/Mobile Documents/com~apple~CloudDocs/生活/家財
 # Generate the intermid file for checking
 fo_csv = open(path+CsvFile, 'r', encoding='utf-8')
 PointsData = csv.DictReader(fo_csv)
-
-with open(path+DictFile,'w') as fw_dict:
-    for row in PointsData:
-        #print(row)
-        fw_dict.write(str(row))
-        fw_dict.write(',\n') 
-fw_dict.close()
 
 # Generate the final Geojson file
 fo_csv = open(path+CsvFile, 'r', encoding='utf-8')   # 还要再写一次，否则下面的程序运行后输出为空，why？
